6db414a61cb3f01e01aff80bd6f241a792a667e1
AbdoRayan404 <gamecorner71@gmail.com>
2023-02-16 13:31:24 +0200

    add: base project files

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'iqraa-task',
+    appId: '1:773273085126:web:98ec2b0dc3a6a2be8ea3c7',
+    storageBucket: 'iqraa-task.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBYMn_zV6rAWEzefQq0vcCXVif-9g8R0_c',
+    authDomain: 'iqraa-task.firebaseapp.com',
+    messagingSenderId: '773273085126',
+    measurementId: 'G-XZQJ6KS6NV',
+  },};

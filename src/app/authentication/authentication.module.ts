5dd0bbb43ce3e59aaf1a1235c87ed7098296419d
AbdoRayan404 <gamecorner71@gmail.com>
2023-02-19 14:52:36 +0200

    remove: unused default module component

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { AuthenticationComponent } from './authentication.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
@@ -10,7 +9,6 @@ import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AuthenticationComponent,
     LoginComponent,
     RegisterComponent
   ],
